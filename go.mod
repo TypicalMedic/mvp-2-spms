789a8d5cb456fa4cf3d321c1031c5bf8fb257a98
Мария Плетнева <=>
2024-02-27 13:00:14 +0500

    added get proj commits handler

--- go.mod
+++ go.mod
@@ -7,9 +7,13 @@ require (
 	gorm.io/gorm v1.25.7
 )
 
-require github.com/go-sql-driver/mysql v1.7.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+)
 
 require (
+	github.com/google/go-github/v56 v56.0.0
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	gorm.io/driver/mysql v1.5.4
--- web_server/handlers/project_handlers.go
+++ web_server/handlers/project_handlers.go
@@ -6,6 +6,9 @@ import (
 	"mvp-2-spms/web_server/handlers/interfaces"
 	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type ProjectHandler struct {
@@ -25,6 +28,22 @@ func (h *ProjectHandler) GetAllProfProjects(w http.ResponseWriter, r *http.Reque
 		ProfessorId: uint(professorId),
 	}
 	result := h.projectInteractor.GetProfessorProjects(input)
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(result)
+}
+
+func (h *ProjectHandler) GetProjectCommits(w http.ResponseWriter, r *http.Request) {
+	professorIdCookie, _ := r.Cookie("professor_id")
+	professorId, _ := strconv.ParseUint(professorIdCookie.Value, 10, 32)
+	projectId, _ := strconv.ParseUint(chi.URLParam(r, "projectID"), 10, 32)
+	from, _ := time.Parse("2006-01-02T15:04:05.000Z", r.URL.Query().Get("from"))
+	input := inputdata.GetProjectCommits{
+		ProfessorId: uint(professorId),
+		ProjectId:   uint(projectId),
+		From:        from,
+	}
+	result := h.projectInteractor.GetProjectCommits(input)
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(result)
