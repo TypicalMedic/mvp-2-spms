db7d62a63c3d2eba137fcaff38a6c438126f1d29
Мария Плетнева <=>
2024-03-03 19:16:47 +0500

    added task model

---
+++ database/models/task.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"fmt"
+	entities "mvp-2-spms/domain-aggregate"
+	"strconv"
+	"time"
+)
+
+type Task struct {
+	Id          uint      `gorm:"column:id"`
+	Name        string    `gorm:"column:name"`
+	Description string    `gorm:"column:description"`
+	Deadline    time.Time `gorm:"column:deadline"`
+	ProjectId   uint      `gorm:"column:project_id"`
+	FolderId    uint      `gorm:"column:folder_id;default:null"`
+	TaskFileId  uint      `gorm:"column:task_file_id;default:null"`
+}
+
+func (*Task) TableName() string {
+	return "task"
+}
+
+func (t *Task) MapToEntity() entities.Task {
+	return entities.Task{
+		Id:          fmt.Sprint(t.Id),
+		Name:        t.Name,
+		Description: t.Description,
+		Deadline:    t.Deadline,
+		ProjectId:   fmt.Sprint(t.ProjectId),
+	}
+}
+
+func (t *Task) MapEntityToThis(entity entities.Task) {
+	mId, _ := strconv.Atoi(entity.Id)
+	prId, _ := strconv.Atoi(entity.ProjectId)
+	t.Id = uint(mId)
+	t.Name = entity.Name
+	t.Description = entity.Description
+	t.ProjectId = uint(prId)
+	t.Deadline = entity.Deadline
+}
