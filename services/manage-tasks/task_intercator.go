061b7eb2a4974ad20c26a6a106525ae12980c661
Мария Плетнева <=>
2024-05-13 17:14:51 +0500

    added error handling to task inter

--- services/manage-tasks/task_intercator.go
+++ services/manage-tasks/task_intercator.go
@@ -1,10 +1,12 @@
 package managetasks
 
 import (
+	"errors"
 	"fmt"
 	"mvp-2-spms/services/interfaces"
 	"mvp-2-spms/services/manage-tasks/inputdata"
 	"mvp-2-spms/services/manage-tasks/outputdata"
+	"mvp-2-spms/services/models"
 
 	"golang.org/x/oauth2"
 )
@@ -25,24 +27,49 @@ func InitTaskInteractor(projRepo interfaces.IProjetRepository, taskRepo interfac
 
 func (p *TaskInteractor) AddTask(input inputdata.AddTask, cloudDrive interfaces.ICloudDrive) (outputdata.AddTask, error) {
 	// add to db
-	task, _ := p.taskRepo.CreateTask(input.MapToTaskEntity())
-	// get project folder id
-	projFolder, _ := p.projectRepo.GetProjectCloudFolderId(fmt.Sprint(input.ProjectId))
-
-	// getting professor drive info, should be checked for existance later
-	driveInfo, _ := p.accountRepo.GetAccountDriveData(fmt.Sprint(input.ProfessorId))
-
-	//////////////////////////////////////////////////////////////////////////////////////////////////////
-	// check for access token first????????????????????????????????????????????
-	token := &oauth2.Token{
-		RefreshToken: driveInfo.ApiKey,
+	task, err := p.taskRepo.CreateTask(input.MapToTaskEntity())
+	if err != nil {
+		return outputdata.AddTask{}, err
 	}
-	cloudDrive.Authentificate(token)
 
-	// add folder to cloud (create folder and task file)
-	driveTask, _ := cloudDrive.AddTaskToDrive(task, projFolder)
-	// add folder id and file id from drive
-	p.taskRepo.AssignDriveTask(driveTask)
+	// get project folder id
+	folderFound := true
+	projFolder, err := p.projectRepo.GetProjectCloudFolderId(fmt.Sprint(input.ProjectId))
+	if err != nil {
+		if !errors.Is(err, models.ErrProjectCloudFolderNotFound) {
+			return outputdata.AddTask{}, err
+		}
+		folderFound = false
+	}
+
+	if folderFound {
+		// getting professor drive info, should be checked for existance later
+		driveInfo, err := p.accountRepo.GetAccountDriveData(fmt.Sprint(input.ProfessorId))
+		if err != nil {
+			return outputdata.AddTask{}, err
+		}
+
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
+		// check for access token first????????????????????????????????????????????
+		token := &oauth2.Token{
+			RefreshToken: driveInfo.ApiKey,
+		}
+
+		err = cloudDrive.Authentificate(token)
+		if err != nil {
+			return outputdata.AddTask{}, err
+		}
+
+		// add folder to cloud (create folder and task file)
+		driveTask, err := cloudDrive.AddTaskToDrive(task, projFolder)
+		if err != nil {
+			return outputdata.AddTask{}, err
+		}
+
+		// add folder id and file id from drive
+		p.taskRepo.AssignDriveTask(driveTask)
+	}
+
 	// returning id
 	output := outputdata.MapToAddTask(task)
 	return output, nil
@@ -50,7 +77,11 @@ func (p *TaskInteractor) AddTask(input inputdata.AddTask, cloudDrive interfaces.
 
 func (p *TaskInteractor) GetProjectTasks(input inputdata.GetProjectTasks) (outputdata.GetProjectTasks, error) {
 	// get tasks from db
-	tasks, _ := p.taskRepo.GetProjectTasksWithCloud(fmt.Sprint(input.ProjectId))
+	tasks, err := p.taskRepo.GetProjectTasksWithCloud(fmt.Sprint(input.ProjectId))
+	if err != nil {
+		return outputdata.GetProjectTasks{}, err
+	}
+
 	output := outputdata.MapToGetProjectTasks(tasks)
 	return output, nil
 }
